f0759d6fc2eaf43b2b5a6e16ff55897d5d32f2ae
Alonna McCallum <alonnavs@gmal.com>
2024-02-26 14:41:39 -0500

    intialize

---
+++ script.js
@@ -0,0 +1,6 @@
+let firstName = "Alonna ";
+let lastName = "McCallum";
+let age = 20;
+alert("My name is " + firstName + lastName);
+alert("I am " + age + " years old");
+console.log("I am having fun in this class, even though I seem to confuse myself a lot.")
